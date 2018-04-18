73b1996cdfe8fab74bc12ba2c0d9ae38d20cc3a3
Irina Hristova <hristova119@gmail.com>
2018-04-18 10:36:52 +0300

    Added UserRoles and Areas

---
+++ iTest/iTest.Infrastructure/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace iTest.Infrastructure
+{
+    public class UserRoles
+    {
+        public const string AdminRole = "Admin";
+        public const string UserRole = "User";
+    }
+}
--- iTest/iTest/Startup.cs
+++ iTest/iTest/Startup.cs
@@ -57,6 +57,14 @@ namespace iTest.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
