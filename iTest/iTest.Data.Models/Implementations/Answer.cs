e0c0f622dcebe58fe3c6aff8acf7261f32b97fed
mkovachev <martin.kovachev@outlook.com>
2018-04-19 19:54:21 +0300

    data models validations added

--- iTest/iTest.Data.Models/Implementations/Answer.cs
+++ iTest/iTest.Data.Models/Implementations/Answer.cs
@@ -1,6 +1,7 @@
 ﻿using iTest.Data.Models.Contracts;
 using Microsoft.TeamFoundation.TestManagement.Client;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace iTest.Data.Models.Implementations
 {
@@ -8,14 +9,20 @@ namespace iTest.Data.Models.Implementations
     {
         public string Id { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime? CreatedOn { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime? ModifiedOn { get; set; }
 
         public bool IsDeleted { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime? DeletedOn { get; set; }
 
+        [Required]
+        [MinLength(4)]
+        [MaxLength(500)]
         public string Description { get; set; }
     }
 }
--- iTest/iTest.Data.Models/Implementations/Test.cs
+++ iTest/iTest.Data.Models/Implementations/Test.cs
@@ -3,6 +3,7 @@ using iTest.Data.Models.Enums;
 using Microsoft.TeamFoundation.TestManagement.Client;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace iTest.Data.Models.Implementations
 {
@@ -10,22 +11,32 @@ namespace iTest.Data.Models.Implementations
     {
         public string Id { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime? CreatedOn { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime? ModifiedOn { get; set; }
 
         public bool IsDeleted { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime? DeletedOn { get; set; }
 
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
         public string Name { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime RequestedTime { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime ExecutionTime { get; set; }
 
+        [Required]
         public Status Status { get; set; }
 
+        [Required]
         public Category Category { get; set; }
 
         public ICollection<Result> Results { get; set; } = new List<Result>();
