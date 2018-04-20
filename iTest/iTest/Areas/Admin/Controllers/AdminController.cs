0143823c9c3011a1c48c9e7ad7f3e731a45fc70e
mkovachev <martin.kovachev@outlook.com>
2018-04-20 11:37:33 +0300

    iTest.Services project added folder structure for admin and user created

---
+++ iTest/iTest/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using iTest.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iTest.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = UserRoles.AdminRole)]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
