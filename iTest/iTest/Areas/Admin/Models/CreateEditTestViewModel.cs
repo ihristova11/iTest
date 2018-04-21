fb56a58da6e10aa948f24c6657634347b8278267
mkovachev <martin.kovachev@outlook.com>
2018-04-21 19:40:31 +0300

    created

---
+++ iTest/iTest/Areas/Admin/Models/CreateEditTestViewModel.cs
@@ -0,0 +1,30 @@
+﻿using iTest.DTO;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace iTest.Web.Areas.Admin.Controllers.Models
+{
+    public class CreateEditTestViewModel
+    {
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? CreatedOn { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime RequestedTime { get; set; }
+
+        [Required]
+        public CategoryDTO Category { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+    }
+}
