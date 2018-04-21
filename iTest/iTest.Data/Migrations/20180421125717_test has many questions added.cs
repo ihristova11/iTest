d80570ee2022560372f7fea41d4556bc582b1ea8
mkovachev <martin.kovachev@outlook.com>
2018-04-21 16:04:47 +0300

    test has many questions

---
+++ iTest/iTest.Data/Migrations/20180421125717_test has many questions added.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace iTest.Data.Migrations
+{
+    public partial class testhasmanyquestionsadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TestId",
+                table: "Questions",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_TestId",
+                table: "Questions",
+                column: "TestId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Tests_TestId",
+                table: "Questions",
+                column: "TestId",
+                principalTable: "Tests",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Tests_TestId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_TestId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "TestId",
+                table: "Questions");
+        }
+    }
+}
