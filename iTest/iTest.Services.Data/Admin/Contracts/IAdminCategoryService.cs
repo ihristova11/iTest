f5e977bebdc72e72d3be4916d58cf3d2dc717e1a
mkovachev <martin.kovachev@outlook.com>
2018-04-21 19:37:03 +0300

    created

---
+++ iTest/iTest.Services.Data/Admin/Contracts/IAdminCategoryService.cs
@@ -0,0 +1,23 @@
+﻿using iTest.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace iTest.Services.Data.Admin.Contracts
+{
+    public interface IAdminCategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> AllAsync();
+
+        Task PublishAsync(CategoryDTO dto);
+
+        Task CreateAsync(string name);
+
+        Task EditAsync(int id, string name);
+
+        Task DeleteAsync(int id);
+
+        Task<bool> ExistsByIdAsync(int id);
+
+        Task<bool> ExistsByNameAsync(string name);
+    }
+}
