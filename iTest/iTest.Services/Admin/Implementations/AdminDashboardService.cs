b5994388cb9fde34e2853e6dcbe176775267ebc4
mkovachev <martin.kovachev@outlook.com>
2018-04-21 12:23:07 +0300

    admin dashboard service created

--- iTest/iTest.Services/Admin/Implementations/AdminDashboardService.cs
+++ iTest/iTest.Services/Admin/Implementations/AdminDashboardService.cs
@@ -1,16 +1,37 @@
-﻿using iTest.Infrastructure.Providers;
+﻿using iTest.Data.Models.Implementations;
+using iTest.Data.Repository.Contracts;
+using iTest.DTO;
+using iTest.Infrastructure.Providers;
 using iTest.Services.Admin.Contracts;
 using System;
+using System.Collections.Generic;
 
 namespace iTest.Services.Admin.Implementations
 {
     public class AdminDashboardService : IAdminDashboardService
     {
         private readonly IMappingProvider mapper;
+        private readonly IRepository<Test> tests;
 
-        public AdminDashboardService(IMappingProvider mapper)
+        public AdminDashboardService(IMappingProvider mapper, IRepository<Test> test)
         {
             this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            this.tests = test ?? throw new ArgumentNullException(nameof(test));
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TestDTO> GetByPostId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Publish(TestDTO dto)
+        {
+            throw new NotImplementedException();
         }
     }
 }
