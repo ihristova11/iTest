5d556978b1d0094d55ade12e107f2b8c13371c54
mkovachev <martin.kovachev@outlook.com>
2018-04-21 12:19:28 +0300

    save changes class created

---
+++ iTest/iTest.Data/Repository/Implementations/EntitySaver.cs
@@ -0,0 +1,16 @@
+﻿using iTest.Data.Repository.Contracts;
+
+namespace iTest.Data.Repository.Implementations
+{
+    public class EntitySaver : ISaver
+    {
+        private readonly iTestDbContext context;
+
+        public EntitySaver(iTestDbContext context) => this.context = context;
+
+        public void SaveChanges() => this.context.SaveChanges();
+
+        public async void SaveChangesAsync() => await this.context.SaveChangesAsync();
+    }
+
+}
