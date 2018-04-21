4c0311d950311ec4338c0ebc3bd4da06a8a4e12a
mkovachev <martin.kovachev@outlook.com>
2018-04-21 10:26:51 +0300

    admin dashboard service created

---
+++ iTest/iTest.Services/Admin/Contracts/IAdminDashboardService.cs
@@ -0,0 +1,12 @@
+﻿using iTest.Infrastructure.Providers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iTest.Services.Admin.Contracts
+{
+    public interface IAdminDashboardService
+    {
+       
+    }
+}
---
+++ iTest/iTest.Services/Admin/Implementations/AdminDashboardService.cs
@@ -0,0 +1,16 @@
+﻿using iTest.Infrastructure.Providers;
+using iTest.Services.Admin.Contracts;
+using System;
+
+namespace iTest.Services.Admin.Implementations
+{
+    public class AdminDashboardService : IAdminDashboardService
+    {
+        private readonly IMappingProvider mapper;
+
+        public AdminDashboardService(IMappingProvider mapper)
+        {
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+    }
+}
