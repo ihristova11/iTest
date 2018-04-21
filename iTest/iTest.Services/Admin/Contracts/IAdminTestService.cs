a85e29581e12633c062dd243f4dd48267351abe3
mkovachev <martin.kovachev@outlook.com>
2018-04-21 15:24:50 +0300

    added admin tests service methods

---
+++ iTest/iTest.Services/Admin/Contracts/IAdminTestService.cs
@@ -0,0 +1,28 @@
+﻿using iTest.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace iTest.Services.Admin.Contracts
+{
+    public interface IAdminTestService
+    {
+        IEnumerable<TestDTO> GetByTestId(int id);
+
+        Task<TestDTO> FindByIdAsync(int id);
+
+        Task<IEnumerable<TestDTO>> AllAsync();
+
+        Task PublishAsync(TestDTO dto);
+
+        Task CreateAsync(string name);
+
+        Task EditAsync(int id, string name);
+
+        Task DeleteAsync(int id);
+
+        bool ExistsById(int id);
+
+        bool ExistsByName(string name);
+
+    }
+}
---
+++ iTest/iTest.Services/Admin/Implementations/AdminTestService.cs
@@ -0,0 +1,70 @@
+﻿using iTest.Data.Models.Implementations;
+using iTest.Data.Repository.Contracts;
+using iTest.DTO;
+using iTest.Infrastructure.Providers;
+using iTest.Services.Admin.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace iTest.Services.Admin.Implementations
+{
+    public class AdminTestService : IAdminTestService
+    {
+        private readonly IMappingProvider mapper;
+        private readonly IRepository<Test> tests;
+        private readonly ISaver saver;
+
+        public AdminTestService(IMappingProvider mapper, IRepository<Test> tests, ISaver saver)
+        {
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            this.tests = tests ?? throw new ArgumentNullException(nameof(tests));
+            this.saver = saver ?? throw new ArgumentNullException(nameof(saver));
+        }
+
+        public Task<IEnumerable<TestDTO>> AllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(int id, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExistsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExistsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TestDTO> FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TestDTO> GetByTestId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PublishAsync(TestDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
