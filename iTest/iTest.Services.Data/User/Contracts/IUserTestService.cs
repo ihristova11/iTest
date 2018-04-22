158b4eebd501f295cc05dc309070c3d9e1d46599
mkovachev <martin.kovachev@outlook.com>
2018-04-22 17:06:45 +0300

    implemented

---
+++ iTest/iTest.Services.Data/User/Contracts/IUserTestService.cs
@@ -0,0 +1,13 @@
+﻿using iTest.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace iTest.Services.Data.User.Contracts
+{
+    public interface IUserTestService
+    {
+        Task<IEnumerable<TestDTO>> AllAsync();
+
+        Task<TestDTO> FindByNameAsync(string name);
+    }
+}
