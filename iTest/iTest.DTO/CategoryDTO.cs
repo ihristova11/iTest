3d541632fb17109318b4f53a88d637e26e731b52
mkovachev <martin.kovachev@outlook.com>
2018-04-29 14:16:16 +0300

    should have the same props to be able to map it

--- iTest/iTest.DTO/CategoryDTO.cs
+++ iTest/iTest.DTO/CategoryDTO.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections.Generic;
-using iTest.Data.Models.Implementations;
+﻿using iTest.Data.Models.Implementations;
+using System;
+using System.Collections.Generic;
 
 namespace iTest.DTO
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
 
         public string Name { get; set; }
 
