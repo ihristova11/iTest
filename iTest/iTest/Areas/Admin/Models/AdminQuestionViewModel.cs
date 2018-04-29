c52cbe4d9c1805ff90e265a5609d01d1228c0598
Irina Hristova <hristova119@gmail.com>
2018-04-29 18:43:47 +0300

    Updated AdminQuestionViewModel and AdminQuestionViewModel

--- iTest/iTest/Areas/Admin/Models/AdminQuestionViewModel.cs
+++ iTest/iTest/Areas/Admin/Models/AdminQuestionViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace iTest.Web.Areas.Admin.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using iTest.Web.Areas.Admin.Models.CustomValidationAttributes;
+
+namespace iTest.Web.Areas.Admin.Models
 {
     public class AdminQuestionViewModel
     {
+        [Required]
+        public string Body { get; set; }
+
+        [Required(ErrorMessage = "Please add Answers to your Question!")]
+        [CollectionCount(2, ErrorMessage = "Please add atleast two Aswers to your Question!")]
+        public IList<AdminAnswerViewModel> Answers { get; set; }
     }
 }
