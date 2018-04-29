2d946c126f44cfac2d093782d3ddcf8d8871e8e9
Irina Hristova <hristova119@gmail.com>
2018-04-29 18:20:23 +0300

    Added custom validation attribute

---
+++ iTest/iTest/Areas/Admin/Models/CustomValidationAttributes/CollectionCount.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace iTest.Web.Areas.Admin.Models.CustomValidationAttributes
+{
+    public class CollectionCount : ValidationAttribute
+    {
+        private int min;
+
+        public CollectionCount(int min)
+        {
+            this.min = min;
+        }
+
+        public override bool IsValid(object value)
+        {
+            var list = value as ICollection;
+            if (list != null)
+            {
+                return list.Count >= this.min;
+            }
+            return false;
+        }
+    }
+}
