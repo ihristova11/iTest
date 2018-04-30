b2c468c6c7f5e2d35bbb597dae55e1216c1836bb
Irina Hristova <hristova119@gmail.com>
2018-04-30 08:23:15 +0300

    Modified AdminQuestionViewModel

--- iTest/iTest/Areas/Admin/Models/AdminQuestionViewModel.cs
+++ iTest/iTest/Areas/Admin/Models/AdminQuestionViewModel.cs
@@ -6,8 +6,12 @@ namespace iTest.Web.Areas.Admin.Models
 {
     public class AdminQuestionViewModel
     {
+        [Required(ErrorMessage = "Please enter the Question's description!")]
+        [StringLength(500, ErrorMessage = "Question description's length must be maximum 500 symbols!")]
+        public string DescriptionPlainText { get; set; }
+
         [Required]
-        public string Body { get; set; }
+        public string Description { get; set; }
 
         [Required(ErrorMessage = "Please add Answers to your Question!")]
         [CollectionCount(2, ErrorMessage = "Please add atleast two Aswers to your Question!")]
