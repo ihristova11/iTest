fd9a7994ad1fdd38be2d0eb77a82c5bed5030bf3
mkovachev <martin.kovachev@outlook.com>
2018-05-01 10:50:31 +0300

    added mapperextension but not needed

---
+++ iTest/iTest/Infrastructure/Extensions/MappingProviderExtension.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace iTest.Web.Infrastructure.Extensions
+{
+    public static class MappingProviderExtension
+    {
+        public static IMappingExpression<TSource, TDest> IgnoreAll<TSource, TDest>(this IMappingExpression<TSource, TDest> e)
+        {
+            e.ForAllMembers(x => x.Ignore());
+            return e;
+        }
+    }
+}
