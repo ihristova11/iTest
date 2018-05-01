ea53148f7933054b49051247ef9c334e5ef4666b
mkovachev <martin.kovachev@outlook.com>
2018-05-01 20:56:22 +0300

    inherits from abstract class DataModel, author changed to required

--- iTest/iTest.Data.Models/Implementations/Test.cs
+++ iTest/iTest.Data.Models/Implementations/Test.cs
@@ -1,34 +1,20 @@
-﻿using iTest.Data.Models.Contracts;
+﻿using iTest.Data.Models.Abstract;
 using iTest.Data.Models.Enums;
 using Microsoft.TeamFoundation.TestManagement.Client;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace iTest.Data.Models.Implementations
 {
-    public class Test : IIdentifiable<int>, IEditable, IDeletable
+    public class Test : DataModel, IIdentifiable<int>, IEditable, IDeletable, IAuditable
     {
-        public int Id { get; set; }
-
-        [DataType(DataType.Date)]
-        public DateTime? CreatedOn { get; set; }
-
-        [DataType(DataType.Date)]
-        public DateTime? ModifiedOn { get; set; }
-
-        public bool IsDeleted { get; set; }
-
-        [DataType(DataType.Date)]
-        public DateTime? DeletedOn { get; set; }
-
         [Required]
         [MinLength(2)]
         [MaxLength(50)]
         public string Name { get; set; }
-        
+
         public int RequestedTime { get; set; }
-        
+
         public int ExecutionTime { get; set; }
 
         public Status Status { get; set; }
@@ -38,6 +24,7 @@ namespace iTest.Data.Models.Implementations
 
         public string AuthorId { get; set; }
 
+        [Required]
         public User Author { get; set; }
 
         public ICollection<UserTest> Users { get; set; } = new List<UserTest>();
