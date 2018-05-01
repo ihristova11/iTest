9fa339902c96e3656cbfb1ded58864fd9433a27a
mkovachev <martin.kovachev@outlook.com>
2018-05-01 21:03:41 +0300

    major update

--- iTest/iTest.Data.Models/Implementations/User.cs
+++ iTest/iTest.Data.Models/Implementations/User.cs
@@ -1,11 +1,24 @@
 ﻿using iTest.Data.Models.Abstract;
-using Microsoft.TeamFoundation.TestManagement.Client;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace iTest.Data.Models.Implementations
 {
-    public class User : DataModel, IIdentifiable<int>, IEditable, IDeletable, IAuditable
+    public class User : IdentityUser, IDeletable, IAuditable
     {
+        public bool IsDeleted { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? DeletedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? CreatedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? ModifiedOn { get; set; }
+
         public ICollection<UserTest> Tests { get; set; } = new List<UserTest>();
     }
 }
