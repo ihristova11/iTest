f93bf8cacdbc06a7f69b3d1bfa9b3bf3b7d73add
mkovachev <martin.kovachev@outlook.com>
2018-05-01 21:33:10 +0300

    removed all not required methods and adapted to the new major updates

--- iTest/iTest/Areas/Admin/Models/AdminCategoryViewModel.cs
+++ iTest/iTest/Areas/Admin/Models/AdminCategoryViewModel.cs
@@ -8,5 +8,7 @@ namespace iTest.Web.Areas.Admin.Models
         [MinLength(2)]
         [MaxLength(50)]
         public string Name { get; set; }
+
+        public string Author { get; set; }
     }
 }
