5985f72fa3fd7297ddf4dae6d678154a37aac02b
mkovachev <martin.kovachev@outlook.com>
2018-05-06 15:21:38 +0300

    mapping of taken test not working!

--- iTest/iTest/wwwroot/js/take-test.js
+++ iTest/iTest/wwwroot/js/take-test.js
@@ -4,59 +4,32 @@
             if ($("#test-form").valid()) {
                 var data = {};
                 data.Questions = [];
-                var questions = $('#questions');
 
-                $.each(questions,
-                    (i, q) => {
-                        let $q = $(q);
-                        let question = {};
+                let questions = $('#questionContainer'); // currQuestion // questionContainer
 
-                        let qContent = $q.find(".question")
+                for (var i = 0; i < questions.length; i++) {
 
-                        question.Description = qContent;
-                        question.Answers = [];
+                    let question = questions[i];
+                    question.Answers = [];
 
-                        let qAnswers = $q.find(".allAnswers");
+                    let answers = $('#answerContainer'); // currAnswer // answerContainer
 
-                        console.log(qAnswers);
+                    for (var j = 0; j < answers.length; j++) {
 
-                        $.each(qAnswers,
-                            (i, a) => {
-                                let $a = $(a);
-                                let answer = {};
+                        let answerIsChecked = $('input:radio[name="currAnswerIsCorrect"]:checked');
 
-                                if ($a.find(".answer").is(":checked")) {
-                                    question.Answers.push(answer);
-                                }
-                            });
-
-                        data.Questions.push(question);
-                    });
-
-
-                //for (var i = 0; i < questions.length; i++) {
-                //    let question = questions[i];
-                //    var answers = $('#allAnswers');
-                //    for (var j = 0; j < answers.length; j++) {
-
-                //        //let selected = $('input:radio[name="answer"]:checked');
-                //        let selected2 = $('.answer').is(":checked")
-
-                //        console.log(selected);
-                //        console.log(selected2);
-
-                //        if (selected2) {
-                //            question.push(answers[j]);
-                //        }
-                //        data.Questions.push(question);
-                //        console.log(data.Questions)
-                //    }
-                //}
+                        if (answerIsChecked) {
+                            question.Answers.push(answers[j]);
+                        }
+                        data.Questions.push(question)
+                    }
+                }
 
                 $.ajax({
                     url: "/users/dashboard/details",
                     type: "POST",
-                    contentType: "application/json",
+                    contentType: "application/json;charset=utf-8",
+                    Accept: "application/json",
                     data: JSON.stringify(data),
                     success: (response) => {
                         window.location.href = response;
