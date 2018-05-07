20e71361bbdcb525d0fd86b8444771ddfbdd0971
Irina Hristova <hristova119@gmail.com>
2018-05-07 09:40:12 +0300

    Adding tests for AdminCategoriesController

---
+++ iTest/iTest.UnitTests/Controllers/Admin/AdminCategoryController/Constructor_Should.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using iTest.Infrastructure.Providers;
+using iTest.Services.Data.Admin.Contracts;
+using iTest.Web.Areas.Admin.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NToastNotify;
+
+namespace iTest.UnitTests.Controllers.Admin.AdminCategoryController
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenMappingProviderIsNull()
+        {
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var testServiceMock = new Mock<IAdminTestService>();
+            var categoriesServiceMock = new Mock<IAdminCategoryService>();
+            var toastrMock = new Mock<IToastNotification>();
+
+            Assert.ThrowsException<ArgumentNullException>(() => new AdminCategoriesController(testServiceMock.Object, categoriesServiceMock.Object, null, toastrMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenCategoryServiceIsNull()
+        {
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var testServiceMock = new Mock<IAdminTestService>();
+            var toastrMock = new Mock<IToastNotification>();
+
+            Assert.ThrowsException<ArgumentNullException>(() => new AdminCategoriesController(testServiceMock.Object, null, mappingProviderMock.Object, toastrMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenTestServiceIsNull()
+        {
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var categoriesServiceMock = new Mock<IAdminCategoryService>();
+            var toastrMock = new Mock<IToastNotification>();
+
+            Assert.ThrowsException<ArgumentNullException>(() => new AdminCategoriesController(null, categoriesServiceMock.Object, mappingProviderMock.Object, toastrMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenToastrIsNull()
+        {
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var testServiceMock = new Mock<IAdminTestService>();
+            var categoriesServiceMock = new Mock<IAdminCategoryService>();
+
+            Assert.ThrowsException<ArgumentNullException>(() => new AdminCategoriesController(testServiceMock.Object, categoriesServiceMock.Object, mappingProviderMock.Object, null));
+        }
+    }
+}
